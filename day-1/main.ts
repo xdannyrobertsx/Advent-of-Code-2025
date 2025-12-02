2ec029f698349215c071c528b76062afb5771513
danny <xdannyrobertsx@gmail.com>
2025-12-02 17:29:11 -0600

    update

--- day-1/main.ts
+++ day-1/main.ts
@@ -4,25 +4,61 @@ export const main = async (filePath: string) => {
   const numberRegex = new RegExp(/\d+/);
   const incrementRegex = new RegExp(/\D+/);
   let currentNumber = 50;
-  let result = 0;
+  let result1 = 0;
+  let result2 = 0;
 
   rotations.forEach((rotation) => {
     const number = Number((numberRegex.exec(rotation) ?? [])[0]);
     const increment = (incrementRegex.exec(rotation) ?? [])[0];
 
+    const initialNum = currentNumber;
+
     const numberState = increment === "L"
       ? (currentNumber - number)
       : (currentNumber + number);
 
-    currentNumber = numberState % 100
+    console.log(
+      "\nrotation",
+      rotation,
+    );
 
-    if (currentNumber === 0) result++;
+    console.log(
+      "initialNum",
+      initialNum,
+    );
+
+    console.log(
+      "numberState",
+      numberState,
+    );
+
+    let danny;
+
+    currentNumber = numberState % 100;
+    console.log("currentNumber", currentNumber)
+
+    if (currentNumber === 0) {
+     danny = 0
+    }
+    else if (currentNumber < 0) {
+        danny = 100 + currentNumber;
+    }
+    else {
+        danny = initialNum - currentNumber;
+    }
+
+    console.log(
+      "danny",
+      danny
+    );
+
+    if (currentNumber === 0) result1++;
   });
 
-  return result;
+  return { result1, result2 };
 };
 
 if (import.meta.main) {
-  const result = await main("input.txt");
-  console.log(result);
+  const { result1 } = await main("input.txt");
+  console.log(`Result 1: ${result1}`);
 }
--- day-1/test.ts
+++ day-1/test.ts
@@ -2,6 +2,7 @@ import { expect } from "@std/expect";
 import { main } from "./main.ts";
 
 Deno.test("get correct output given example", async () => {
-  const result = await main("example.txt");
-  expect(result).toBe(3);
+  const { result1, result2 } = await main("example.txt");
+  expect(result1).toBe(3);
+  expect(result2).toBe(6);
 });
