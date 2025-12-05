580b7bfb58ae703b0b990e99d236fbc3538d4ac8
danny <xdannyrobertsx@gmail.com>
2025-12-04 21:25:04 -0600

    part 1

--- day-3/main.ts
+++ day-3/main.ts
@@ -1,45 +1,35 @@
 const getLineNumber = (line: string): number => {
-  // for each line, match two, load it if second is larger, check next num
   const nums = line.split("").map(Number);
   let maxNum: number = 0;
   let maxNum2: number = 0;
-  let maxNumIndex: number = 0;
+  let _maxNumIndex: number = 0;
   let maxNumIndex2: number = 0;
 
   nums.forEach((num, index) => {
-
     if (num < maxNum && num < maxNum2) return;
-
-    else if (num > maxNum) {
+    else if (num > maxNum && (index + 1) < nums.length) {
       maxNum = num;
-      maxNumIndex = index;
+      _maxNumIndex = index;
       maxNum2 = 0;
       maxNumIndex2 = 0;
 
       return;
     } else if (num > maxNum2 && index > maxNumIndex2) {
-
       maxNum2 = num;
       maxNumIndex2 = index;
       return;
     }
   });
-  console.log("\n\nhere!");
-  console.log("line", line);
-  console.log("maxNum", maxNum);
-  console.log("maxNum2", maxNum2);
-  console.log("maxNumIndex", maxNumIndex);
-  console.log("maxNumIndex2", maxNumIndex2);
 
-  return 1;
+  const numString = `${maxNum}${maxNum2}`;
+  return Number(numString);
 };
 
 export const main = async (filePath: string) => {
   const input = await Deno.readTextFile(filePath);
   const lines = input.split("\n");
-  console.log(lines[3]);
   const numArray = lines.map(getLineNumber);
-  return;
+  return numArray.reduce((acc, num) => acc + num);
 };
 
 if (import.meta.main) {
