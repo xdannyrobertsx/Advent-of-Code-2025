5ca17695c24909f6e94be1a879cc01e213dfe6b5
danny <xdannyrobertsx@gmail.com>
2025-12-16 22:47:15 -0600

    reducer

--- day-10/main.ts
+++ day-10/main.ts
@@ -1,6 +1,21 @@
+type Machine = {
+  light: string;
+  buttons: string;
+};
+
 export const main = async (filePath: string) => {
   const input = await Deno.readTextFile(filePath);
-  return input.split(" ").length;
+  const textLines = input.split("\n");
+  const machines = textLines.reduce((acc: Machine[], line: string) => {
+    const [lIndex1, lIndex2] = [line.indexOf("["), line.indexOf("]")];
+    const [bIndex1, bIndex2] = [line.indexOf("("), line.lastIndexOf(")")];
+    const light = line.slice(lIndex1, lIndex2 + 1);
+    const buttons = line.slice(bIndex1, bIndex2 + 1);
+    acc.push({ light, buttons });
+    return acc;
+  }, []);
+  console.log("\nmachines\n", machines);
+  return 7;
 };
 
 if (import.meta.main) {
